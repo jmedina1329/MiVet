0d9e51d5dc63ebdccbdb9ff7d16982c732ea9188
Joe Medina <112432330+jmedina1329@users.noreply.github.com>
2022-12-26 14:34:10 -0500

    Update and rename ShareStory/Story.cs to ShareStory/.NET/Story.cs

---
+++ ShareStory/.NET/Story.cs
@@ -0,0 +1,14 @@
+public class Story
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string StoryText { get; set; }
+    public List<UploadedFile> StoryFile { get; set; }
+    public int CreatedBy { get; set; }
+    public bool IsApproved { get; set; }
+    public int ApprovedBy { get; set; }
+    public DateTime DateCreated { get; set; }
+    public DateTime DateModified { get; set; }
+}
+
