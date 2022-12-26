095111bba75a874883e49c3d5dcc5b210d92c547
Joe Medina <112432330+jmedina1329@users.noreply.github.com>
2022-12-26 14:32:50 -0500

    Update and rename ShareStory/ShareStoryApiController.cs to ShareStory/.NET/ShareStoryApiController.cs

---
+++ ShareStory/.NET/ShareStoryApiController.cs
@@ -0,0 +1,175 @@
+[Route("api/stories")]
+[ApiController]
+public class ShareStoryApiController : BaseApiController
+{
+    private IShareStoryService _service = null;
+    private IAuthenticationService<int> _authService = null;
+    public ShareStoryApiController(
+        IShareStoryService service,
+        ILogger<PingApiController> logger,
+        IAuthenticationService<int> authService) : base(logger)
+    {
+        _service = service;
+        _authService = authService;
+    }
+
+    [HttpPost]
+    public ActionResult<int> Create(StoryAddRequest model)
+    {
+        int code = 200;
+        BaseResponse response = null;
+
+        try
+        {
+            int userId = _authService.GetCurrentUserId();
+
+            int id = _service.Create(model, userId);
+
+            code = 201;
+            response = new ItemResponse<int>()
+            {
+                Item = id
+            };
+        }
+        catch (Exception ex)
+        {
+            code = 500;
+            response = new ErrorResponse(ex.Message);
+            base.Logger.LogError(ex.ToString());
+        }
+        return StatusCode(code, response);
+    }
+
+    [HttpGet]
+    public ActionResult<ItemResponse<Paged<Story>>> GetPaginated(int pageIndex, int pageSize)
+    {
+        int code = 200;
+        BaseResponse response = null;
+
+        try
+        {
+            Paged<Story> paged = _service.GetPaginated(pageIndex, pageSize);
+            if (paged == null)
+            {
+                code = 404;
+                response = new ErrorResponse("Records not found");
+            }
+            else
+            {
+                response = new ItemResponse<Paged<Story>>()
+                {
+                    Item = paged
+                };
+            };
+        }
+        catch (Exception ex)
+        {
+            code = 500;
+            response = new ErrorResponse(ex.Message);
+            base.Logger.LogError(ex.ToString());
+        }
+        return StatusCode(code, response);
+    }
+
+    [HttpGet("{id:int}")]
+    public ActionResult<ItemResponse<Story>> GetById(int id)
+    {
+        int code = 200;
+        BaseResponse response = null;
+
+        try
+        {
+            Story story = _service.GetById(id);
+
+            if (story == null)
+            {
+                code = 404;
+                response = new ErrorResponse("Record not found");
+            }
+            else
+            {
+                response = new ItemResponse<Story>()
+                {
+                    Item = story
+                };
+            }
+        }
+        catch (Exception ex)
+        {
+            code = 500;
+            response = new ErrorResponse(ex.Message);
+            base.Logger.LogError(ex.ToString());
+        }
+        return StatusCode(code, response);
+    }
+
+    [HttpPut("{id:int}")]
+    public ActionResult<SuccessResponse> Update(StoryUpdateRequest model)
+    {
+        int code = 200;
+        BaseResponse response = null;
+
+        try
+        {
+            int userId = _authService.GetCurrentUserId();
+
+            _service.Update(model, userId);
+
+            response = new SuccessResponse();
+        }
+        catch (Exception ex)
+        {
+            code = 500;
+            response = new ErrorResponse(ex.Message);
+            base.Logger.LogError(ex.ToString());
+        }
+        return StatusCode(code, response);
+    }
+
+    [HttpPut("{id:int}/approval/{isApproved:bool}")]
+    public ActionResult<SuccessResponse> UpdateApproval(int id, bool isApproved)
+    {
+        int code = 200;
+        BaseResponse response = null;
+
+        try
+        {
+            int userId = _authService.GetCurrentUserId();
+
+            _service.UpdateApproval(id, userId, isApproved);
+
+            response = new SuccessResponse();
+        }
+        catch (Exception ex)
+        {
+            code = 500;
+            response = new ErrorResponse(ex.Message);
+            base.Logger.LogError(ex.ToString());
+        }
+        return StatusCode(code, response);
+    }
+
+    [HttpDelete("{id:int}")]
+    public ActionResult<SuccessResponse> Delete(int id)
+    {
+        int code = 200;
+        BaseResponse response = null;
+
+        try
+        {
+            int userId = _authService.GetCurrentUserId();
+
+            _service.Delete(id, userId);
+            response = new SuccessResponse();
+
+        }
+        catch (Exception ex)
+        {
+            code = 500;
+            response = new ErrorResponse(ex.Message);
+            base.Logger.LogError(ex.ToString());
+        }
+        return StatusCode(code, response);
+    }
+
+}
