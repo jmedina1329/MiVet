c676dba18a3c16999b2c5bc595e5c386f5e40d5c
Joe Medina <112432330+jmedina1329@users.noreply.github.com>
2022-12-26 14:35:05 -0500

    Update StoryUpdateRequest.cs

--- ShareStory/StoryUpdateRequest.cs
+++ ShareStory/StoryUpdateRequest.cs
@@ -1,16 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Sabio.Models.Requests.ShareStory
+public class StoryUpdateRequest : StoryAddRequest, IModelIdentifier
 {
-    public class StoryUpdateRequest : StoryAddRequest, IModelIdentifier
-    {
-        [Required]
-        [Range(1, Int32.MaxValue)]
-        public int Id { get; set; }
-    }
+    [Required]
+    [Range(1, Int32.MaxValue)]
+    public int Id { get; set; }
 }
+
